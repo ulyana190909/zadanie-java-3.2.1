62860b399ae58807963dbc94d3809e68352fe661
Uliana Utina <ulyana190909@yandex.ru>
2020-07-18 01:08:11 +0300

    kdjf

---
+++ mili.java
@@ -0,0 +1,13 @@
+public class bonusavia {
+    public static void main(String[] args) {
+        float airfare = 10600f;
+        float mili = 0.2f;
+        float bonus = airfare / mili / 100;
+        System.out.println(bonus);
+    }
+}
+
+"C:\Program Files\Java\jdk-11.0.7\bin\java.exe" -javaagent:C:\Users\79269\AppData\Local\JetBrains\Toolbox\apps\IDEA-C\ch-0\201.7846.76\lib\idea_rt.jar=59753:C:\Users\79269\AppData\Local\JetBrains\Toolbox\apps\IDEA-C\ch-0\201.7846.76\bin -Dfile.encoding=UTF-8 -classpath C:\Users\79269\IdeaProjects\untitled\out\production\untitled bonusavia
+530.0
+
+Process finished with exit code 0
