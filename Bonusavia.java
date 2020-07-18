9e8c29f3eb8d7f08b165564486abce5eee375158
Uliana Utina <ulyana190909@yandex.ru>
2020-07-18 13:24:10 +0300

    finish commit

---
+++ Bonusavia.java
@@ -0,0 +1,7 @@
+public class Bonusavia {
+    public static void main(String[] args) {
+        float airfare = 10600f;
+        float mili = 0.2f;
+        float bonus = airfare / mili / 100;
+        System.out.println(bonus); }
+}
