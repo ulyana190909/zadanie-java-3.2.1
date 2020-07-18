bfc6acde9de23d7f95062e7a32e6921f4bb76b0d
Uliana Utina <ulyana190909@yandex.ru>
2020-07-18 20:40:10 +0300

    корректирование кода

--- Bonusavia.java
+++ Bonusavia.java
@@ -1,7 +1,8 @@
 public class Bonusavia {
     public static void main(String[] args) {
-        float airfare = 10600f;
-        float mili = 0.2f;
-        float bonus = airfare / mili / 100;
-        System.out.println(bonus); }
+        float Airfare = 10600f;
+        float Mili = 0.2f;
+        float Bonus = Airfare / Mili / 100;
+        System.out.println(Bonus);
+    }
 }
