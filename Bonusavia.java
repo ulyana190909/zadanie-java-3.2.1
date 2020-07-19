2d642dc94f5c7baf63ccb4483870e99a07ec6afd
Uliana Utina <ulyana190909@yandex.ru>
2020-07-19 10:08:25 +0300

    исправление названия переменных

--- Bonusavia.java
+++ Bonusavia.java
@@ -1,8 +1,8 @@
 public class Bonusavia {
     public static void main(String[] args) {
-        float Airfare = 10600f;
-        float Mili = 0.2f;
-        float Bonus = Airfare / Mili / 100;
-        System.out.println(Bonus);
+        float airfare = 10600f;
+        float mili = 0.2f;
+        float bonus = airfare / mili / 100;
+        System.out.println(bonus);
     }
 }
